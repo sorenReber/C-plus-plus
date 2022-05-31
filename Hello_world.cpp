c25297de59f17674babde552bf084a869d09fc9e
sorenReber <reb19002@byui.edu>
2022-05-31 00:47:59 -0700

    Initial Commit

---
+++ Hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! The complier was too much of a pain to get going in Visual Studio Code" << endl;
+  cout << "I could not get it to work so I'm trying code::blocks instead" << endl;
+  cout << "Which was 1000 times easier to get to work." << endl;
+  cout << "I will use Visual Studio Code just to update GitHub" << endl;
+  return 0;
+}
